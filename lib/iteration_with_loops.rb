27ad0178b5894597e2bf257b4e553fe039b0348a
Kendall Stephens <kendoll.stephens@gmail.com>
2020-07-04 15:02:34 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,9 +1,9 @@
 def find_min_in_nested_arrays(src)
-  array_of_daily_temperature = []
+  array_of_daily_temp = []
   row_index = 0
   while row_index < src.count do 
-    array_of_daily_temperature.push(src[row_index.min])
+    array_of_daily_temp.push(src[row_index.min])
  row_index +=1
  end
- array_of_daily_temperature
+ array_of_daily_temp
  end
